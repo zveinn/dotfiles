aa54725547de402458700d2c566a46f8c97a391c
zveinn <info@sveinn.is>
2025-03-18 17:26:27 +0000

    update

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,6 +8,7 @@ local config = {
 	hide_tab_bar_if_only_one_tab = true,
 	font_size = 12,
 	window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+	window_decorations = "TITLE | RESIZE | INTEGRATED_BUTTONS",
 	window_background_opacity = 1,
 	-- foreground_text_hsb = {
 	-- 	hue = 1,
@@ -51,7 +52,7 @@ end
 
 config.colors = { foreground = "#abb2bf", background = "#080808" }
 -- config.font = wezterm.font('Hurmit Nerd Font Mono', { weight = 'Medium', italic = false })
-config.font = wezterm.font('Berkeley Mono', { weight = 'Medium', italic = false })
+config.font = wezterm.font('Hurmit Nerd Font Mono', { weight = 'Medium', italic = false })
 
 -- wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
 -- 	local xx = pane.current_working_dir
