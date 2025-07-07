ac96ff024fa0665eafa3df414398b9efe7a97f3d
zveinn <info@sveinn.is>
2025-07-07 08:11:45 -0500

    Testing helix from master branch

--- test/main.go
+++ test/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
-	fmt.Println(os.Args)
-	fmt.Println("this is my custom program")
-	os.Exit(0)
+	filepath.WalkDir(os.Args[1], func(path string, d fs.DirEntry, err error) error {
+		if strings.Contains(path, "node") {
+			return filepath.SkipDir
+		}
+		if strings.Contains(path, ".git") {
+			return filepath.SkipDir
+		}
+		fmt.Println(path)
+		return nil
+	})
 }
